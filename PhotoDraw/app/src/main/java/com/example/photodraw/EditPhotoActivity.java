f1295df891f2dd44c0183f985eb1d217cc800d1b
Dima Kopyl <dimakopyl22@gmail.com>
2022-07-26 13:02:38 +0300

    Update #1
    - Add main code

---
+++ PhotoDraw/app/src/main/java/com/example/photodraw/EditPhotoActivity.java
@@ -0,0 +1,64 @@
+package com.example.photodraw;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+import java.io.File;
+
+public class EditPhotoActivity extends AppCompatActivity {
+
+    ImageView imgeit;
+    String path = "";
+    Uri inputImageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_photo);
+        imgeit = (ImageView) findViewById(R.id.imgeit);
+
+        path = getIntent().getExtras().getString("path");
+        File imgFile = new File(path);
+        if (imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            imgeit.setImageBitmap(myBitmap);
+        }
+
+        inputImageUri = Uri.fromFile(new File(path));
+        edit_trial();
+
+    }
+
+    public void edit_trial () {
+        Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class );
+        dsPhotoEditorIntent.setData(inputImageUri);
+
+        // set edited image to directory
+        dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY,"Photo Directory");
+        int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION,DsPhotoEditorActivity.TOOL_CROP };
+
+        dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE,toolsToHide);
+        startActivityForResult(dsPhotoEditorIntent,200);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_OK) {
+            switch (requestCode) {
+                case 200 : Uri outputUri = data.getData();
+                // set Edited Image
+                imgeit.setImageURI(outputUri);
+                break;
+            }
+        }
+    }
+}
---
+++ PhotoDraw/app/src/main/java/com/example/photodraw/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.photodraw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgV_camera;
+
+    // Request Permission
+    private final int REQUEST_CODE_PERMISSION = 101;
+    private final String[] REQUEST_PERMISSION = new String[]{
+            "android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.READ_EXTERNAL_STORAGE",
+            //"android.permission.MANAGE_EXTERNAL_STORAGE"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgV_camera = (ImageView) findViewById(R.id.imgV_camera);
+
+        if(allPermissionGranted()){
+
+        } else {
+            ActivityCompat.requestPermissions(this,REQUEST_PERMISSION,REQUEST_CODE_PERMISSION);
+        }
+
+        imgV_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private boolean allPermissionGranted() {
+            for (String permission : REQUEST_PERMISSION) {
+                if (ContextCompat.checkSelfPermission(this,permission)!= PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+
+                // Android 11 >
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && false == Environment.isExternalStorageManager()) {
+                    Uri uri = Uri.parse("package:" + BuildConfig.APPLICATION_ID);
+                    startActivity(new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri));
+                }
+            }
+            return true;
+    }
+
+}
